172eb3d5145c0695f30f89abb7905673bc6688d0
Adrian Bercovici <adriansan_93@yahoo.com>
2021-08-05 06:01:52 +0300

    finished implementing the global cache. Might change for Pids in the future

--- apps/epc_sgw/src/epc_sgw_session_cache.erl
+++ apps/epc_sgw/src/epc_sgw_session_cache.erl
@@ -1,27 +1,50 @@
 -module(epc_swg_session_cache).
--export([start/0,getSession/2]).
-
+-behaviour(gen_event).
+-export([start_link/0,init/1,handle_event/2,handle_call/2]).
+-define(NAME,?MODULE).
 
 -record(session,{
-    uid,
     pid=not_set,
     ref=not_set,
     route
 }).
+-record(state,{
+    dict
+}).
 
-start()->
-    {ok,dict:new()}.
+start_link()->
+    {ok,Pid}=gen_event:start_link({global,?NAME},[]),
+    {ok,Pid}.
+
+init(Args)->
+    {ok,#state{dict=dict:new()}}.
+
+% -- callbacks
+handle_event({create_session,Uid},State)->
+    NewDict=create_session(Uid,State#state.dict),
+    {ok,State#state{dict=NewDict}};
+
+handle_event({register_session,{Uid,Ref,Pid}},State=#state{dict=Dict})->
+    NewDict=register_session({Uid,Ref,Pid}, Dict),
+    {ok,State#state{dict=NewDict}}.
+
+handle_call(Message,State)->
+    {ok,rr,State}.
+
+create_session(Uid,Dict)->
+    create_option(dict:find(Uid,Dict),Uid,Dict).
+create_option({ok,Value},Uid,Dict)->Dict;
+create_option(error,Uid,Dict)->
+    dict:store(Uid,#session{}, Dict).
+
+register_session({Uid,Ref,Pid},Dict)->
+    dict:update(Uid,fun(Old)->Old#session{ref=Ref,pid=Pid} end, Dict).
+
+
+
     
 
-getSession(Uid,Dict)->
-    {ok,Record}=findUserData(Uid,Dict).
 
-
-findUserData(Uid,Dict)->
-    case dict:find(Uid,Dict) of
-        {ok,Record}->Record;
-        _-> throw("Undefined user")
-    end.
 
     
     
