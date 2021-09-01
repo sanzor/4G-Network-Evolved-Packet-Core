70f8d410c2b9d87cc837b18a97b0036d4aba311f
Adrian Bercovici <adriansan_93@yahoo.com>
2021-09-01 14:25:14 +0300

    almost finished the epc_client

--- apps/epc_client/src/epc_client_app.erl
+++ apps/epc_client/src/epc_client_app.erl
@@ -1,43 +1,68 @@
 -module(epc_client_app).
 -behaviour(application).
--export([start/1]).
+-export([start/2,stop/1]).
+-record(state,{
+    socket,
+    printerRef
+}).
 
 
 
 start(normal,[])->
-    {ok,Pid}=epc_mme_main_sup:start_link(),
+     Pid=spawn(fun()->start()end),
     {ok,Pid}.
-
-start({takeover,_OtherNode})->
-    {ok,Pid}=epc_mme_main_sup:start_link(),
-    Pid.
-
 stop(Reason)->ok.
+start()->
+    epc_mme_server:authorize(application:get_env(epc_client,userId)),
+    ClientPid=start_client(),
+    ClientPid.
 
-
-
-loop(Socket)->
-    receive 
-        {tcp,Message,Socket} -> 
-            handle_message(Message,Socket),
-            loop(Socket);
-        {tcp_closed,_}->handle_close()
+logger_loop(File)->
+    receive
+        Message -> io:format(File,"Received ~p",[Message]),
+                   io:format("Received ~p",[Message]),
+                   logger_loop(File)
     end.
 
+loop(State)->
+    receive
+        {tcp,v,Id}->
+                Data=term_to_binary({verify,Id}),
+                gen_tcp:send(State#state.socket,Data,Data),
+                loop(State);
+        {'DOWN',_Ref,process,_PrinterPID,Reason}->
+                io:format("Printer is down, reason:[~p]",[Reason]),
+                exit(normal);
+        {tcp,Socket,Message} -> 
+            handle_message(Message,State),
+            loop(Socket);
+        {tcp_closed,_Socket}->exit(socket_closed)
+    end.
 
-handle_close()->undefined.
-handle_message(Message,Socket)->undefined.
-connect()->
-    {ok,UserId}=application:get_env(userid),
-    epc_mme_api:authorize(UserId),
-    Socket=make_connection(),
-    loop(Socket).
+start_client()->
+    Socket=make_socket_connection(),
+    PrinterRef=start_logger_process(),
+    ClientPid=spawn(fun()->loop(#state{printerRef=PrinterRef,socket=Socket})end),
+    register(cl,ClientPid),
+    ClientPid.
+    
+    
+start_logger_process()->
+    {ok,Handle}=file:open(application:get_env(epc_client,printerPath), [write]),
+    register(printer,spawn(fun()->logger_loop(Handle)end)),
+    Ref=erlang:monitor(process,whereis(printer)),
+    Ref.
+
+handle_message(Message,_State)->
+    printer ! Message.
 
 
-make_connection()->
-    {ok,PortNr}=application:get_env(port_nr),
-    {ok,Address}=application:get_env(address),
-    {ok,Socket}=gen_tcp:connect(Address, PortNr, []),
+make_socket_connection()->
+
+    {ok,Socket}=gen_tcp:connect(
+                               application:get_env(epc_client,connectPort),
+                               application:get_env(epc_client,address),
+                                []),
     Socket.
 
      
