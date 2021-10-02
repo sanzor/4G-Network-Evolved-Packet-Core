a4f39fe4ff7af75d3b1531fe0cb7656da643b40b
Adrian Bercovici <adriansan_93@yahoo.com>
2021-10-02 11:15:07 +0300

    connecting works

--- apps/epc_client/src/epc_client_app.erl
+++ apps/epc_client/src/epc_client_app.erl
@@ -8,7 +8,6 @@
 }).
 -define(DB(X),io:format("~p",[X])).
 -define(EN(Key,Config),proplists:get_value(Key, Config)).
-
 start(normal,[])->
     Pid=spawn(fun()->start()end),
     {ok,Pid}.
@@ -24,7 +23,7 @@ start_client()->
     Config=application:get_all_env(),
    
     ClientPid=spawn(fun()-> startC(Config,self())end),
-    ClientPid ! {self(),{epc_client,verify,?EN(userId, Config)}},
+    ClientPid ! {epc_client,verify,?EN(userId, Config)},
     receive
         MSG->?DB({got_from_loop,MSG})
     after 1500 ->
@@ -38,28 +37,31 @@ start_client()->
   
 
 startC(Config,SpawnerPid)->
+   
     {_Something,_UserId}=epc_mme_api:authorize({?EN(userId, Config),
                                                 ?EN(username, Config),
                                                 ?EN(phoneNumber, Config)}),
-    Socket=gen_tcp:connect(?EN(address,Config),?EN(port,Config),[binary]),
+  
+    {ok,Socket}=gen_tcp:connect(?EN(address,Config),?EN(connectPort,Config),[binary]),
+   
     PrinterRef=start_logger_process(Config),
     loop(#state{printerRef=PrinterRef,socket=Socket,spawner=SpawnerPid}).
 
 
-loop(State=#state{spawner=From})->
+loop(State)->
     
     receive
-        {From,{epc_client,verify,Id}}->
+        {epc_client,verify,Id}->
                 Data=term_to_binary({verify,Id}),
-                From ! {ok,verified,Id},
                 gen_tcp:send(State#state.socket,Data),
                 loop(State);
         {'DOWN',_Ref,process,_PrinterPID,Reason}->
                 io:format("Printer is down, reason:[~p]",[Reason]),
                 exit({normal,Reason});
-        {tcp,Socket,Message} -> 
-            handle_message(Message,State),
-            loop(Socket);
+        {tcp,_Socket,Raw} -> 
+            Message=binary_to_term(Raw),
+            NewState=handle_socket_message(Message,State),
+            loop(NewState);
         {tcp_closed,_Socket}->
             exit(socket_closed);
         MSG->?DB({unknown,MSG}),
@@ -90,8 +92,10 @@ logger_loop(File)->
                    logger_loop(File)
     end.
 
-handle_message(Message,_State)->
-    io:format("got message ~p",[binary_to_term(Message)]).
+handle_socket_message(Message,_State)->
+     ?DB({{state,_State},{message,Message}}),
+     _State#state.spawner ! Message,
+    _State.
 
 
 
