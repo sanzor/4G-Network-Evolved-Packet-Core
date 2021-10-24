54a094b814e14c75b86277b969e6c6042d78a5a4
Adrian Bercovici <adriansan_93@yahoo.com>
2021-10-24 23:22:04 +0300

    to fix the reply problem

--- apps/epc_client/src/epc_client_server.erl
+++ apps/epc_client/src/epc_client_server.erl
@@ -1,13 +1,14 @@
 -module(epc_client_server).
 -behaviour(gen_statem).
 -export([login/1,quit/0,connect/1,start_link/0]).
--export([handle_call/3,handle_info/2,handle_cast/2,init/1]).
-
+-export([init/1,handle_event/3,callback_mode/0]).
+-export([logged_out/3,logged_in_not_verified/3,waiting_verification/3]).
 -record(state,{
     userId,
     userName,
     phone,
-    authorized=false
+    authorized=false,
+    socket
 }).
 -define(NAME,?MODULE).
 -define(VERIFY_CONNECT_TIMEOUT,5000).
@@ -17,7 +18,7 @@ start_link()->
     gen_statem:start_link({local,?NAME},?MODULE, [],[]).
 
 login(Data)->
-    gen_statem:call({local,?NAME}, Data).
+    gen_statem:call(?NAME,{login,Data},3000).
 
 quit()->
     gen_statem:cast({local,?NAME},quit).
@@ -37,21 +38,34 @@ logged_out({call,_From},{quit,_},_State)->
 logged_out({call,_From},{login,{UserId,Username,PhoneNumber}},State)->
     Return= try 
     {_Something,_UserId}=epc_mme_api:authorize({UserId,Username,PhoneNumber}),
+    gen_statem:reply(?NAME,{ok,logged_in,UserId}),
     {next_state,logged_in_not_verified,State#state{userId=UserId,userName=Username,phone=PhoneNumber}}
     catch
-        _:_ -> {keep_state,logged_out,State}
+        Error:Reason -> 
+        gen_statem:reply(?NAME,{error,Error,Reason}),
+        {keep_state,logged_out,State}
     end,
     Return.
 
-logged_in_not_verified(_,_,State=#state{userId=UserId})->
-    {keep_state,logged_in_not_verified,State}.
-
-
-
-handle_event({call,From},{connect,Address,Port},State)->
+% event that comes on user pressing connect (already logged in)
+logged_in_not_verified({call,_From},{connect,Address,Port},State)->
     {ok,Socket}=gen_tcp:connect(Address,Port,[binary]),
-    erlang:send_after(?VERIFY_CONNECT_TIMEOUT, self(), {received,verify_ack}),
-    {} % move to new state and check the timeout !
+    erlang:send_after(?VERIFY_CONNECT_TIMEOUT, self(), {send_after_message,verify_ack}),
+    {next_state,waiting_verification,State#state{socket=Socket},[]}. % move to new state and check the timeout !
+
+waiting_verification({call,_From},{tcp,_Socket,_Raw},State)->
+    {keep_state,waiting_verification,State#state{authorized=true}}.
+
+% event that comes triggered by erlang:send_after
+% checks if client was verified
+handle_event({call,_From},{send_after,verify_ack},State=#state{authorized=A})->
+    NewState= if A=:=true -> connected ; true -> logged_in_not_verified end,
+    {next_state,NewState,State};
+
+handle_event({call,From},Msg,State)->
+    gen_statem:reply(From,{unkown_msg,Msg}),
+    {keep_state,State}.
+
     
 
 
